d60eb29458e5c52bc5babd1e7adf4c3559b93453
Siddhant Vishnu <siddhant.m.vishnu@gmail.com>
2024-11-25 12:29:26 +0530

    Removed Protected

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+/* import React, { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 
@@ -16,9 +16,4 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   return <>{children}</>;
 };
 
-export default ProtectedRoute;
-On branch main
-Your branch and 'origin/main' have diverged,
-and have 2 and 6 different commits each, respectively.
-
-nothing to commit, working tree clean
+export default ProtectedRoute; */
